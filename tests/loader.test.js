6837186d5be61173978e0f1744642075896d400d
Othmane Bakhtaoui <UO259323@uniovi.es>
2019-03-19 17:47:38 +0100

    adding codecov

---
+++ tests/loader.test.js
@@ -0,0 +1,15 @@
+/**
+ * author: es6a2 group
+ */
+const assert = require('assert');
+const Loader = require('../src/lib/Repositories/SolidLoaderRepository');
+const auth = require('solid-auth-client');
+
+describe('Loader', function() {
+  it('loading chat and interlocutor', async function() {
+    const loader = new Loader(auth.fetch);
+    const chat = await loader.loadFromUrl('https://morningstar.solid.community/public/dechat_201903160752.ttl#jtbuliv7', 'https://morningstar.solid.community/profile/card#me', 'https://morningstar.solid.community/public/dechat_201903160752.ttl');
+    assert.equal(chat.getUrl(), 'https://morningstar.solid.community/public/dechat_201903160752.ttl#jtbuliv7', 'The url of the chat is not correct.' + chat.getUrl());
+    assert.equal(chat.userWebId, 'https://morningstar.solid.community/profile/card#me', 'The WebId of the user is not correct : ' + chat.userWebId);
+  });
+});
