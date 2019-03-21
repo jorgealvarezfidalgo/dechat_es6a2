9bb5d5a0765ad513b9f5faf51387765c363a486a
Othmane Bakhtaoui <UO259323@uniovi.es>
2019-03-21 12:51:53 +0100

    checking coverage

--- tests/service.test.js
+++ tests/service.test.js
@@ -19,22 +19,29 @@ const createService = new CreateService(auth.fetch);
 
 const loader = new Loader(auth.fetch);
 
-describe('Services tests', function () {
+describe('Services', function () {
 
     it('checking the picture and name are correct', async function () {
         const chat = await loader.loadChatFromUrl('https://othbak.solid.community/public/unittest_201903201125.ttl#jth2a2sl', 'https://othbak.solid.community/profile/card#me', 'https://othbak.solid.community/public/unittest_201903201125.ttl');
 
-        assert.equal(await baseService.getPhoto(chat.userWebId), null, 'The user does not have a photo ');
+        const selfPhoto = await baseService.getPhoto(chat.userWebId);
+        assert.equal(selfPhoto, null, 'The user does not have a photo : ' + chat.userWebId + ' ->' + selfPhoto);
 
-        assert.equal(await baseService.getFormattedName(chat.userWebId) , 'Othmane Bakhtaoui', 'The user name is not correct');
+        const name = await baseService.getFormattedName(chat.userWebId);
+        assert.equal(name, 'Othmane Bakhtaoui', 'The user name is not correct : ->' + name);
+
+        const note =   await baseService.getNote(chat.userWebId);
+        assert.equal(note, null, 'we do not have a note yet.');
+
 
     });
 
     it('checking the number of messages is 23', async function () {
         const chat = await loader.loadChatFromUrl('https://othbak.solid.community/public/unittest_201903201125.ttl#jth2a2sl', 'https://othbak.solid.community/profile/card#me', 'https://othbak.solid.community/public/unittest_201903201125.ttl');
         //opening messages
+        const chats = await openService.getChatsToOpen(chat.userWebId);
         //the user for the moment have 23 messages
-        assert.equal( (await openService.getChatsToOpen(chat.userWebId)).length, 23, 'the number of messages is not correct : ' + (await openService.getChatsToOpen(chat.userWebId)).length);
+        assert.equal(chats.length, 23, 'the number of messages is not correct : ' + chats.length);
     });
 
     it('checking the inboxUrl', async function () {
@@ -44,7 +51,9 @@ describe('Services tests', function () {
         inboxUrls[chat.userWebId] = (await baseService.getObjectFromPredicateForResource(chat.userWebId, namespaces.ldp + 'inbox')).value;
         assert.equal(inboxUrls[chat.userWebId], 'https://othbak.solid.community/inbox/', 'the inbox url is not correct : ' + inboxUrls[chat.userWebId]);
 
-        assert.equal( await baseService.getInboxUrl(chat.userWebId), inboxUrls[chat.userWebId], 'the inbox url is not correct : ' + inboxUrls[chat.userWebId]);
+        expectedUrl = await baseService.getInboxUrl(chat.userWebId);
+        assert.equal(inboxUrls[chat.userWebId], expectedUrl, 'the inbox url is not correct : ' + inboxUrls[chat.userWebId]);
+
     });
 
     it('checking that there are 4 messages in my pod', async function () {
@@ -57,7 +66,7 @@ describe('Services tests', function () {
     it('checking writing permissions', async function () {
         const chat = await loader.loadChatFromUrl('https://othbak.solid.community/public/unittest_201903201125.ttl#jth2a2sl', 'https://othbak.solid.community/profile/card#me', 'https://othbak.solid.community/public/unittest_201903201125.ttl');
         const dataUrl = baseService.getDefaultDataUrl(chat.userWebId);
-        assert.equal(await baseService.writePermission( baseService.getDefaultDataUrl(chat.userWebId)), false, 'we do not have writing permission for the moment ');
+        assert.equal(await baseService.writePermission(dataUrl), false, 'we do not have writing permission for the moment ');
     });
 
     it('creating individual semantic chat', async function () {
@@ -67,10 +76,16 @@ describe('Services tests', function () {
         const friendName = await baseService.getFormattedName('https://morningstar.solid.community/profile/card#me');
 
         //simulating a new chat
-        assert.equal(await baseService.getFormattedName('https://morningstar.solid.community/profile/card#me'), 'Luci', 'The user name is not correct : ->' + friendName);
+        assert.equal(friendName, 'Luci', 'The user name is not correct : ->' + friendName);
         assert.equal(semanticChat.userWebId, 'https://othbak.solid.community/profile/card#me', 'The user web id is not correct : ->' + semanticChat.userWebId);
         assert.equal(semanticChat.getMessages().length, 0, 'we do not have messages yet : ' + semanticChat.getMessages().length);
         assert.equal(semanticChat.interlocutorWebId, 'https://morningstar.solid.community/profile/card#me', 'Thefriend web id is not correct : ->' + semanticChat.userWebId);
+
+        //getting interlocutor
+        let interlocutor = baseService.getInterlocutor(semanticChat.fileurl, 'https://othbak.solid.community/profile/card#me')
+        //the same user because he sent the message
+        assert.equal(interlocutor.length, null , 'The interlocutor is not correct : ->' + interlocutor.length);
+
 
     });
 
@@ -116,6 +131,7 @@ describe('Services tests', function () {
         if (message) {
             newMessageFound = true;
         }
+
         convoToJoin = await joinService.getJoinRequest(chat.fileurl, chat.userWebId, joinService);
         const dataUrl = baseService.getDefaultDataUrl(chat.userWebId);
         //this should do nothing
@@ -123,5 +139,10 @@ describe('Services tests', function () {
 
         assert.equal(newMessageFound, false, 'the user does not have pending invitations.');
         assert.equal(convoToJoin, null, 'the user does not have convos to join. ->' + convoToJoin);
+        assert.equal(messageService.getChatOfMessage(message).messageText,null, 'the user does not have pending invitations');
+        const username = await baseService.getFormattedName('https://othbak.solid.community/profile/card#me');
+
+        //simulating a store message test and should return error
+        messageService.storeMessage(chat.fileurl, username, 'https://othbak.solid.community/profile/card#me', '11:20', 'hello', 'https://morningstar.solid.community/profile/card#me', 'hello', null);
     });
 });
