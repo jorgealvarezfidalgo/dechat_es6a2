0ed6eb71de292a5b927936dd70da3413ec717b05
Othmane Bakhtaoui <UO259323@uniovi.es>
2019-03-27 20:26:45 +0100

    now accepting popup windows

--- features/google-search.feature
+++ features/google-search.feature
@@ -1,8 +1,12 @@
 #Search.feature
 
-Feature: Google Search Testing
-    I should be able to go to a website
-    and check its search functionality
+Feature: DeChat ES-6A-II
+    I should be able to go to the webpage and login
+
+    Scenario: Good credential, we reach the home page
+        Given We visit the "https://arquisoft.github.io/dechat_es6a2"
+        And We put the good credentials username "asw" and password "asw-2018-19"
+        Then We click and we reach the home page with the messages shown and contacts
 
     Scenario: Google search for voter cards app
         When I search Google for "itunes vote cards app"
--- step-definitions/google-search-steps.js
+++ step-definitions/google-search-steps.js
@@ -7,10 +7,45 @@ module.exports = function () {
             .then(() => {
                 return page.googleSearch.performSearch(text)
             })
-    })
+    });
 
     this.Then(/^I should see "([^"]*)" in the result$/, function (keywords) {
         return driver.wait(until.elementsLocated(by.partialLinkText(keywords)), 10000);
     });
+
+    //_______________________ DECHAT - ES6A - II ___________________________//
+
+    this.Given(/^We visit the "([^"]*)"$/, function (arg1) {
+          return helpers.loadPage(arg1)
+            .then(() => {
+              return driver.wait(until.elementsLocated(by.xpath('//*[@id="nav-login-btn"]')), 10000)
+               .then(function (elements) {
+                   expect(elements.length).to.not.equal(0);
+               });
+            })
+       });
+
+       this.Given(/^We put the good credentials username "([^"]*)" and password "([^"]*)"$/, function (arg1, arg2) {
+         return helpers.loadPage("https://arquisoft.github.io/dechat_es6a2")
+           .then(() => {
+             return driver.findElement(by.xpath('//*[@id="nav-login-btn"]')).click()
+             .then(() => {
+               driver.manage().timeouts().implicitlyWait(10);
+                 return driver.getAllWindowHandles()
+                 .then(function gotWindowHandles(allHandles) {
+                    driver.manage().timeouts().implicitlyWait(10);
+                     driver.switchTo().window(allHandles[allHandles.length -1]);
+                     driver.manage().timeouts().implicitlyWait(10);
+                     return driver.findElement(by.xpath('//*[@id="app-container"]/div/div/button[2]'))
+
+                 })
+             })
+         })
+      });
+
+   this.Then(/^We click and we reach the home page with the messages shown and contacts$/, function (callback) {
+   // Write code here that turns the phrase above into concrete actions
+   callback(null, 'pending');
+ });
 
 };
