4eb66f92af099dd5b89010f92432ccfbd94548db
Othmane Bakhtaoui <UO259323@uniovi.es>
2019-03-28 14:02:24 +0100

    finishing cucumber tests with 5 scenarios and 16 steps

--- features/dechat-es6a-II.feature
+++ features/dechat-es6a-II.feature
@@ -19,12 +19,12 @@ Feature: DeChat ES-6A-II
         Then the messages will appear and we an existing conversation
         Then we send the implicated friend a message "hi friend"
 
-    Scenario: When the user is logged in, we select a friend and write a message
+    Scenario: When the user is logged in, we should see friends and add contacts/groups icons
         Given We visit the "https://arquisoft.github.io/dechat_es6a2"
         And We put the credentials username "asw" and password "asw-2018-19" and click on "login"
         Then the messages will appear and we select a friend in the friends' section
-        Then create a new conversation and write a message "why hello there"
+        Then we should see friends and add groups and friends menu
 
-    Scenario: Google search for arquisoft
-        When I search Google for "arquisoft"
-        Then I should see "GitHub" in the result
+    Scenario: Google search for our application es6a-II
+        When I search Google for "dechat_es6a2"
+        Then I should see "Arquisoft" in the result
--- step-definitions/dechat-steps.js
+++ step-definitions/dechat-steps.js
@@ -166,34 +166,81 @@ module.exports = function () {
     this.Then(/^we send the implicated friend a message "([^"]*)"$/, function (arg1) {
         //'hello' should appear
         return driver.findElement(by.xpath('//*[@id="chatwindow0"]/div[1]/div/h1')).click()
-        .then(() => {
-          return driver.wait(until.elementsLocated(by.xpath('//*[@id="chatdiv"]/div/div[2]')), 20000)
-              .then(() => {
-                  driver.findElement(by.xpath('//*[@id="write-chat"]')).sendKeys(arg1)
-                  return driver.findElement(by.xpath('//*[@id="write-chat"]')).sendKeys(webdriver.Key.ENTER)
-                      .then(() => {
-                          //new message should appear
-                          return driver.wait(until.elementsLocated(by.xpath('//*[@id="chatdiv"]/div/div[3]')), 20000)
-                      })
-              })
-        })
+            .then(() => {
+                return driver.wait(until.elementsLocated(by.xpath('//*[@id="chatdiv"]/div/div[2]')), 20000)
+                    .then(() => {
+                        driver.findElement(by.xpath('//*[@id="write-chat"]')).sendKeys(arg1)
+                        return driver.findElement(by.xpath('//*[@id="write-chat"]')).sendKeys(webdriver.Key.ENTER)
+                            .then(() => {
+                                //new message should appear
+                                return driver.wait(until.elementsLocated(by.xpath('//*[@id="chatdiv"]/div/div[3]')), 20000)
+                            })
+                    })
+            })
     });
 
-    //______________________________ FOURTH SCENARIO __ CREATING A NEW CONVERSATION ____________________________//
+    //______________________________ FOURTH SCENARIO __ controlling the app apearance ____________________________//
 
-    this.Given(/^We put the credentials username "([^"]*)" and password "([^"]*)" and click on "([^"]*)"$/, function (arg1, arg2, arg3, callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
+    this.Given(/^We put the credentials username "([^"]*)" and password "([^"]*)" and click on "([^"]*)"$/, function (arg1, arg2, arg3) {
+        var parent = driver.getWindowHandle();
+        return helpers.loadPage("https://arquisoft.github.io/dechat_es6a2")
+            .then(() => {
+                return driver.findElement(by.xpath('//*[@id="nav-login-btn"]')).click()
+                    .then(() => {
+                        driver.manage().timeouts().implicitlyWait(10);
+                        return driver.getAllWindowHandles()
+                            .then(function gotWindowHandles(allHandles) {
+                                driver.manage().timeouts().implicitlyWait(10);
+                                driver.switchTo().window(allHandles[allHandles.length - 1]);
+                                driver.manage().timeouts().implicitlyWait(10);
+                                return driver.findElement(by.xpath('/html/body/div/div/div/button[2]')).click()
+                                    .then(() => {
+                                        driver.wait(until.elementsLocated(by.name('username')), 10000);
+                                        driver.findElement(by.name('username')).sendKeys(arg1);
+                                        driver.findElement(by.name('password')).sendKeys(arg2);
+                                        driver.manage().timeouts().implicitlyWait(10);
+                                        return driver.findElement(by.xpath('//*[@id="' + arg3 + '"]')).click()
+                                            .then(() => {
+                                                driver.manage().timeouts().implicitlyWait(10);
+                                                driver.switchTo().window(parent);
+                                                return driver.wait(until.elementsLocated(by.xpath('//*[@id="user-name"]')), 20000);
+                                            })
+                                    })
+
+                            })
+                    })
+            })
     });
 
-    this.Then(/^the messages will appear and we select a friend in the friends' section$/, function (callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
+    this.Then(/^the messages will appear and we select a friend in the friends' section$/, () => {
+        //username correct
+        return driver.wait(until.elementsLocated(by.xpath('//*[@id="user-name"]')), 20000)
+            .then(() => {
+                //selfphoto default is present
+                return driver.wait(until.elementsLocated(by.xpath('//*[@id="selfphoto"]')), 20000)
+                    .then(() => {
+                        //the user has only one conversion with Othmane Bakhtaoui
+                        return driver.wait(until.elementsLocated(by.xpath('//*[@id="show-contacts"]')), 20000);
+                    })
+            });
     });
 
-    this.Then(/^create a new conversation and write a message "([^"]*)"$/, function (arg1, callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback(null, 'pending');
+    this.Then(/^we should see friends and add groups and friends menu$/, () => {
+        //'hello' should appear
+        return driver.wait(until.elementsLocated(by.id('show-contacts')), 25000)
+            .then(() => {
+                return driver.wait(until.elementsLocated(by.xpath('//*[@id="create-group"]')), 25000)
+                    .then(() => {
+                        //see only one conversation
+                        return driver.wait(until.elementsLocated(by.xpath('/html/body/div/div[3]/section[1]/div[3]/div/div[1]')), 20000)
+                            .then(() => {
+                                return driver.wait(until.elementsLocated(by.xpath('//*[@id="interlocutorw-name"]')), 20000)
+                                //search menu is active
+                                    .then(() => {
+                                        return driver.wait(until.elementsLocated(by.xpath('/html/body/div/div[3]/section[1]/div[2]/div[1]/input')), 20000);
+                                    })
+                            })
+                    })
+            })
     });
-
 };
