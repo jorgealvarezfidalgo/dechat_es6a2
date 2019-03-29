47989ffd5bdc5263c513c3e8cd68b2be439100d3
Jorge A. Fidalgo <uo258524@uniovi.es>
2019-03-29 23:33:06 +0100

    Fixing cucumber quotes

--- step-definitions/dechat-steps.js
+++ step-definitions/dechat-steps.js
@@ -5,23 +5,23 @@ module.exports = function () {
 
     //________________________ FIRST CUCUMBER GOOGLE SEARCH _______________________//
 
-    this.When(/^I search Google for "([^"]*)"$/, (text) => {
+    this.When(/^I search Google for "([^']*)"$/, (text) => {
         return helpers.loadPage("https://google.com")
             .then(() => {
                 return page.googleSearch.performSearch(text)
             })
     });
 
-    this.Then(/^I should see "([^"]*)" in the result$/, function (keywords) {
+    this.Then(/^I should see "([^']*)" in the result$/, function (keywords) {
         return driver.wait(until.elementsLocated(by.partialLinkText(keywords)), 10000);
     });
 
     //_______________________ DECHAT - TESTS - ES6A - II ___________________________//
 
-    this.Given(/^We visit the "([^"]*)"$/, function (arg1) {
+    this.Given(/^We visit the "([^']*)"$/, function (arg1) {
         return helpers.loadPage(arg1)
             .then(() => {
-                return driver.wait(until.elementsLocated(by.xpath("//*[@id="nav-login-btn"]")), 10000)
+                return driver.wait(until.elementsLocated(by.xpath("//*[@id='nav-login-btn']")), 10000)
                     .then(function (elements) {
                         expect(elements.length).to.not.equal(0);
                     });
@@ -30,11 +30,11 @@ module.exports = function () {
 
     //______________________________ FIRST SCENARIO __ BAD LOGIN __________________________________//
 
-    this.Given(/^We put the bad credentials username "([^"]*)" and password "([^"]*)"$/, function (arg1, arg2) {
+    this.Given(/^We put the bad credentials username "([^']*)" and password "([^']*)"$/, function (arg1, arg2) {
         var parent = driver.getWindowHandle();
         return helpers.loadPage("https://arquisoft.github.io/dechat_es6a2")
             .then(() => {
-                return driver.findElement(by.xpath("//*[@id="nav-login-btn"]")).click()
+                return driver.findElement(by.xpath("//*[@id='nav-login-btn']")).click()
                     .then(() => {
                         driver.manage().timeouts().implicitlyWait(10);
                         return driver.getAllWindowHandles()
@@ -48,7 +48,7 @@ module.exports = function () {
                                         driver.findElement(by.name("username")).sendKeys(arg1);
                                         driver.findElement(by.name("password")).sendKeys(arg2);
                                         driver.manage().timeouts().implicitlyWait(10);
-                                        return driver.findElement(by.xpath("//*[@id="login"]"))
+                                        return driver.findElement(by.xpath("//*[@id='login']"))
                                     })
 
                             })
@@ -56,9 +56,9 @@ module.exports = function () {
             })
     });
 
-    this.Then(/^We click on "([^"]*)" we will stay on the same page and no messages shown$/, function (arg1) {
+    this.Then(/^We click on "([^']*)" we will stay on the same page and no messages shown$/, function (arg1) {
         //error paragraph
-        return driver.findElement(by.xpath("//*[@id="" + arg1 + ""]")).click()
+        return driver.findElement(by.xpath("//*[@id='" + arg1 + "']")).click()
             .then(() => {
                 driver.manage().timeouts().implicitlyWait(10);
                 return driver.wait(until.elementsLocated(by.xpath("/html/body/div/div[2]/p")), 40000);
@@ -67,11 +67,11 @@ module.exports = function () {
 
     //______________________________ SECOND SCENARIO __ CORRECT LOGIN __________________________________//
 
-    this.Given(/^We put the good credentials username "([^"]*)" and password "([^"]*)"$/, function (arg1, arg2) {
+    this.Given(/^We put the good credentials username "([^']*)" and password "([^']*)"$/, function (arg1, arg2) {
         var parent = driver.getWindowHandle();
         return helpers.loadPage("https://arquisoft.github.io/dechat_es6a2")
             .then(() => {
-                return driver.findElement(by.xpath("//*[@id="nav-login-btn"]")).click()
+                return driver.findElement(by.xpath("//*[@id='nav-login-btn']")).click()
                     .then(() => {
                         driver.manage().timeouts().implicitlyWait(10);
                         return driver.getAllWindowHandles()
@@ -85,11 +85,11 @@ module.exports = function () {
                                         driver.findElement(by.name("username")).sendKeys(arg1);
                                         driver.findElement(by.name("password")).sendKeys(arg2);
                                         driver.manage().timeouts().implicitlyWait(10);
-                                        return driver.findElement(by.xpath("//*[@id="login"]")).click()
+                                        return driver.findElement(by.xpath("//*[@id='login']")).click()
                                             .then(() => {
                                                 driver.manage().timeouts().implicitlyWait(10);
                                                 driver.switchTo().window(parent);
-                                                return driver.wait(until.elementsLocated(by.xpath("//*[@id="user-name"]")), 20000);
+                                                return driver.wait(until.elementsLocated(by.xpath("//*[@id='user-name']")), 20000);
                                             })
                                     })
 
@@ -100,18 +100,18 @@ module.exports = function () {
 
     this.Then(/^We click and we reach the home page with the messages shown$/, () => {
         //username correct
-        return driver.wait(until.elementsLocated(by.xpath("//*[@id="user-name"]")), 10000)
+        return driver.wait(until.elementsLocated(by.xpath("//*[@id='user-name']")), 10000)
             .then(() => {
                 //selfphoto default is present
-                return driver.findElement(by.xpath("//*[@id="selfphoto"]"))
+                return driver.findElement(by.xpath("//*[@id='selfphoto']"))
                     .then(() => {
                         //the user has only one conversion with Othmane Bakhtaoui
-                        driver.wait(until.elementsLocated(by.xpath("//*[@id="chatwindow0"]/div[1]/div/h1")), 20000);
+                        driver.wait(until.elementsLocated(by.xpath("//*[@id='chatwindow0']/div[1]/div/h1")), 20000);
                         //click on the friend
-                        return driver.findElement(by.xpath("//*[@id="chatwindow0"]/div[1]/div/h1")).click()
+                        return driver.findElement(by.xpath("//*[@id='chatwindow0']/div[1]/div/h1")).click()
                             .then(() => {
                                 //"hello" should appear
-                                return driver.wait(until.elementsLocated(by.xpath("//*[@id="chatdiv"]/div/div[2]")), 20000);
+                                return driver.wait(until.elementsLocated(by.xpath("//*[@id='chatdiv']/div/div[2]")), 20000);
                             })
                     })
             });
@@ -119,11 +119,11 @@ module.exports = function () {
 
     //______________________________ THIRD SCENARIO __ SENDING MESSAGE TO AN EXISTING CONVERSATION ________________________//
 
-    this.Given(/^We put the good credentials username "([^"]*)" and password "([^"]*)" and click on "([^"]*)"$/, function (arg1, arg2, arg3) {
+    this.Given(/^We put the good credentials username "([^']*)" and password "([^']*)" and click on "([^']*)"$/, function (arg1, arg2, arg3) {
         var parent = driver.getWindowHandle();
         return helpers.loadPage("https://arquisoft.github.io/dechat_es6a2")
             .then(() => {
-                return driver.findElement(by.xpath("//*[@id="nav-login-btn"]")).click()
+                return driver.findElement(by.xpath("//*[@id='nav-login-btn']")).click()
                     .then(() => {
                         driver.manage().timeouts().implicitlyWait(10);
                         return driver.getAllWindowHandles()
@@ -137,11 +137,11 @@ module.exports = function () {
                                         driver.findElement(by.name("username")).sendKeys(arg1);
                                         driver.findElement(by.name("password")).sendKeys(arg2);
                                         driver.manage().timeouts().implicitlyWait(10);
-                                        return driver.findElement(by.xpath("//*[@id="" + arg3 + ""]")).click()
+                                        return driver.findElement(by.xpath("//*[@id='" + arg3 + "']")).click()
                                             .then(() => {
                                                 driver.manage().timeouts().implicitlyWait(10);
                                                 driver.switchTo().window(parent);
-                                                return driver.wait(until.elementsLocated(by.xpath("//*[@id="user-name"]")), 20000);
+                                                return driver.wait(until.elementsLocated(by.xpath("//*[@id='user-name']")), 20000);
                                             })
                                     })
 
@@ -152,28 +152,28 @@ module.exports = function () {
 
     this.Then(/^the messages will appear and we an existing conversation$/, () => {
         //username correct
-        return driver.wait(until.elementsLocated(by.xpath("//*[@id="user-name"]")), 20000)
+        return driver.wait(until.elementsLocated(by.xpath("//*[@id='user-name']")), 20000)
             .then(() => {
                 //selfphoto default is present
-                return driver.wait(until.elementsLocated(by.xpath("//*[@id="selfphoto"]")), 20000)
+                return driver.wait(until.elementsLocated(by.xpath("//*[@id='selfphoto']")), 20000)
                     .then(() => {
                         //the user has only one conversion with Othmane Bakhtaoui
-                        return driver.wait(until.elementsLocated(by.xpath("//*[@id="chatwindow0"]/div[1]/div/h1")), 20000);
+                        return driver.wait(until.elementsLocated(by.xpath("//*[@id='chatwindow0']/div[1]/div/h1")), 20000);
                     })
             });
     });
 
-    this.Then(/^we send the implicated friend a message "([^"]*)"$/, function (arg1) {
+    this.Then(/^we send the implicated friend a message "([^']*)"$/, function (arg1) {
         //"hello" should appear
-        return driver.findElement(by.xpath("//*[@id="chatwindow0"]/div[1]/div/h1")).click()
+        return driver.findElement(by.xpath("//*[@id='chatwindow0']/div[1]/div/h1")).click()
             .then(() => {
-                return driver.wait(until.elementsLocated(by.xpath("//*[@id="chatdiv"]/div/div[2]")), 20000)
+                return driver.wait(until.elementsLocated(by.xpath("//*[@id='chatdiv']/div/div[2]")), 20000)
                     .then(() => {
-                        driver.findElement(by.xpath("//*[@id="write-chat"]")).sendKeys(arg1)
-                        return driver.findElement(by.xpath("//*[@id="write-chat"]")).sendKeys(webdriver.Key.ENTER)
+                        driver.findElement(by.xpath("//*[@id='write-chat']")).sendKeys(arg1)
+                        return driver.findElement(by.xpath("//*[@id='write-chat']")).sendKeys(webdriver.Key.ENTER)
                             .then(() => {
                                 //new message should appear
-                                return driver.wait(until.elementsLocated(by.xpath("//*[@id="chatdiv"]/div/div[3]")), 20000)
+                                return driver.wait(until.elementsLocated(by.xpath("//*[@id='chatdiv']/div/div[3]")), 20000)
                             })
                     })
             })
@@ -181,11 +181,11 @@ module.exports = function () {
 
     //______________________________ FOURTH SCENARIO __ controlling the app apearance ____________________________//
 
-    this.Given(/^We put the credentials username "([^"]*)" and password "([^"]*)" and click on "([^"]*)"$/, function (arg1, arg2, arg3) {
+    this.Given(/^We put the credentials username "([^']*)" and password "([^']*)" and click on "([^']*)"$/, function (arg1, arg2, arg3) {
         var parent = driver.getWindowHandle();
         return helpers.loadPage("https://arquisoft.github.io/dechat_es6a2")
             .then(() => {
-                return driver.findElement(by.xpath("//*[@id="nav-login-btn"]")).click()
+                return driver.findElement(by.xpath("//*[@id='nav-login-btn']")).click()
                     .then(() => {
                         driver.manage().timeouts().implicitlyWait(10);
                         return driver.getAllWindowHandles()
@@ -199,11 +199,11 @@ module.exports = function () {
                                         driver.findElement(by.name("username")).sendKeys(arg1);
                                         driver.findElement(by.name("password")).sendKeys(arg2);
                                         driver.manage().timeouts().implicitlyWait(10);
-                                        return driver.findElement(by.xpath("//*[@id="" + arg3 + ""]")).click()
+                                        return driver.findElement(by.xpath("//*[@id='" + arg3 + "']")).click()
                                             .then(() => {
                                                 driver.manage().timeouts().implicitlyWait(10);
                                                 driver.switchTo().window(parent);
-                                                return driver.wait(until.elementsLocated(by.xpath("//*[@id="user-name"]")), 20000);
+                                                return driver.wait(until.elementsLocated(by.xpath("//*[@id='user-name']")), 20000);
                                             })
                                     })
 
@@ -214,13 +214,13 @@ module.exports = function () {
 
     this.Then(/^the messages will appear and we select a friend in the friends" section$/, () => {
         //username correct
-        return driver.wait(until.elementsLocated(by.xpath("//*[@id="user-name"]")), 20000)
+        return driver.wait(until.elementsLocated(by.xpath("//*[@id='user-name']")), 20000)
             .then(() => {
                 //selfphoto default is present
-                return driver.wait(until.elementsLocated(by.xpath("//*[@id="selfphoto"]")), 20000)
+                return driver.wait(until.elementsLocated(by.xpath("//*[@id='selfphoto']")), 20000)
                     .then(() => {
                         //the user has only one conversion with Othmane Bakhtaoui
-                        return driver.wait(until.elementsLocated(by.xpath("//*[@id="show-contacts"]")), 20000);
+                        return driver.wait(until.elementsLocated(by.xpath("//*[@id='show-contacts']")), 20000);
                     })
             });
     });
@@ -229,15 +229,15 @@ module.exports = function () {
         //"hello" should appear
         return driver.wait(until.elementsLocated(by.id("show-contacts")), 25000)
             .then(() => {
-                return driver.wait(until.elementsLocated(by.xpath("//*[@id="create-group"]")), 25000)
+                return driver.wait(until.elementsLocated(by.xpath("//*[@id='create-group']")), 25000)
                     .then(() => {
                         //see only one conversation with Othmane Bakhtaoui
-                        return driver.wait(until.elementsLocated(by.xpath("//*[@id="chatwindow0"]/div[1]/div/h1")), 20000)
+                        return driver.wait(until.elementsLocated(by.xpath("//*[@id='chatwindow0']/div[1]/div/h1")), 20000)
                             .then(() => {
-                                return driver.wait(until.elementsLocated(by.xpath("//*[@id="interlocutorw-name"]")), 20000)
+                                return driver.wait(until.elementsLocated(by.xpath("//*[@id='interlocutorw-name']")), 20000)
                                 //show contact information
                                     .then(() => {
-                                        return driver.wait(until.elementsLocated(by.xpath("//*[@id="show-contact-information"]")), 20000);
+                                        return driver.wait(until.elementsLocated(by.xpath("//*[@id='show-contact-information']")), 20000);
                                     })
                             })
                     })
