d4b1c90bcc994e6a6dd78ff87853365712c63334
Jorge A. Fidalgo <uo258524@uniovi.es>
2019-03-30 11:55:51 +0100

    Fixing webdriver

--- step-definitions/dechat-steps.js
+++ step-definitions/dechat-steps.js
@@ -1,6 +1,6 @@
 const expect = require("chai").expect;
-var driver = require("selenium-webdriver");
-var by = driver.By;
+var webdriver = require("selenium-webdriver");
+var by = webdriver.By;
 
 module.exports = function () {
 
